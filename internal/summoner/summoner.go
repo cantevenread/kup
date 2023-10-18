fb6170c3e0163a50bf5fd02d9f5a00b8e66454b8
CanRead <78627491+cantevenread@users.noreply.github.com>
2023-10-17 23:25:26 -0400

    Update summoner.go

--- internal/summoner/summoner.go
+++ internal/summoner/summoner.go
@@ -5,3 +5,19 @@ type Summoner struct {
 	Name  string `json:"name"`
 	PuuID string `json:"puuid"`
 }
+
+func (s *Summoner) UpdateName(name string) {
+	s.Name = name
+}
+
+func (s *Summoner) PullName() string {
+	return s.Name
+}
+
+func (s *Summoner) PullID() string {
+	return s.ID
+}
+
+func (s *Summoner) PullPUUID() string {
+	return s.PuuID
+}
